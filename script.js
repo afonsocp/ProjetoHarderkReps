deaf0abb3bd70f533d382a4136c5f40ebca0a1a6
calabresos20 <afonsopereira880@gmail.com>
2025-04-29 18:17:39 -0300

    Icone

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('searchInput');
     const productCards = document.querySelectorAll('.product-card');
     const sections = document.querySelectorAll('section');
